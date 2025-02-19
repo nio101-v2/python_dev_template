a054d542dd55a62573f820102d6b491e21994ded
nio <nio101@outlook.com>
2025-02-19 07:10:01 +0100

    added commit datetime to version

--- update_version.py
+++ update_version.py
@@ -12,13 +12,16 @@ updates a local _version_.txt file with the git version info
 
 from rich.console import Console
 from git import Repo
+import arrow
 
 # =======================================================
 # main loop
 
 if __name__ == "__main__":
     console = Console()
+
     repo = Repo(search_parent_directories=True)
+    commit_date = arrow.get(repo.commit("main").committed_date).to('local')
     if len(repo.tags) == 0:
         tag = r'\[no_tag]'
         # note : we are escaping the first bracket because :
@@ -26,7 +29,14 @@ if __name__ == "__main__":
     else:
         tag = r'\[' + str(repo.tags[0]) + r']'
     version_string = (
-        str(repo.head.reference) + ' ' + tag + ' #' + repo.head.object.hexsha
+        str(repo.head.reference)
+        + ' '
+        + tag
+        + ' #'
+        + repo.head.object.hexsha[0:4]
+        + ' ('
+        + str(commit_date)
+        + ')'
     )
     console.print(version_string, style="orange_red1", highlight=False)
     with open('./_version_.txt', 'w') as version_file:
